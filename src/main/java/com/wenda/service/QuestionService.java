fa3c9afe52864b868b6b99dcb5ecc62e1a6de8c7
yinfly <ren987@yeah.net>
2018-04-26 16:12:39 +0800

    Initial commit

---
+++ src/main/java/com/wenda/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.wenda.service;
+
+import com.wenda.dao.QuestionDAO;
+import com.wenda.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDAO questionDAO;
+
+    public List<Question> getLatestQuestions(int userId, int offset, int limit) {
+        return questionDAO.selectLatestQuestions(userId, offset, limit);
+    }
+}
---
+++ src/main/java/com/wenda/service/UserService.java
@@ -0,0 +1,24 @@
+package com.wenda.service;
+
+import com.wenda.dao.UserDAO;
+import com.wenda.model.User;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+
+@Service
+public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    @Autowired
+    private UserDAO userDAO;
+
+    public User getUser(int id) {
+        return userDAO.selectById(id);
+    }
+
+}
